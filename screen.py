294a34180c7b7da9b5370832bd0b4efb0ee10117
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-02-21 19:13:53 +0100

    Add count_score method.

--- screen.py
+++ screen.py
@@ -15,7 +15,7 @@ class Screen:
         self.window.minsize(150, 150)
         self.window.config(padx=30, pady=10, bg="#000000")
 
-        self.label = Label()
+        self.generated_text = Text()
         self.entry_field = Entry()
         self.entry = Entry()
         self.timer_text = None
@@ -42,19 +42,19 @@ class Screen:
             words = list(map(str, all_text.split()))
             for word in range(20):
                 self.set_of_words.append(random.choice(words))
-            self.label = Text(self.window, fg="#fafafa", bg="#000000", font=("Arial", 20, "bold"),
-                              height=4, width=50, wrap="word")
-            self.label.grid(column=0, row=2, rowspan=3, columnspan=4)
-            self.label.insert(END, self.change_into_txt(self.set_of_words).lower())
+            self.generated_text = Text(self.window, fg="#fafafa", bg="#000000", font=("Arial", 20, "bold"),
+                                       height=4, width=50, wrap="word")
+            self.generated_text.grid(column=0, row=2, rowspan=3, columnspan=4)
+            self.generated_text.insert(END, self.change_into_txt(self.set_of_words).lower())
 
     def create_text_hard(self):
         self.clear_screen()
         for word in range(20):
             self.set_of_words.append(random.choice(list(get_english_words_set(['web2'], lower=True))))
-        self.label = Text(self.window, fg="#fafafa", bg="#000000", font=("Arial", 20, "bold"),
-                          height=5, width=50, wrap="word")
-        self.label.grid(column=0, row=2, rowspan=3, columnspan=4)
-        self.label.insert(END, self.change_into_txt(self.set_of_words))
+        self.generated_text = Text(self.window, fg="#fafafa", bg="#000000", font=("Arial", 20, "bold"),
+                                   height=5, width=50, wrap="word")
+        self.generated_text.grid(column=0, row=2, rowspan=3, columnspan=4)
+        self.generated_text.insert(END, self.change_into_txt(self.set_of_words))
 
     def change_into_txt(self, list_of_words):
         text = ' '.join(list_of_words)
@@ -64,7 +64,7 @@ class Screen:
     def clear_screen(self):
         self.set_of_words.clear()
         self.text = ""
-        self.label.destroy()
+        self.generated_text.destroy()
         self.spelling.clear()
         self.entry_field.delete(0, END)
         self.spelling_points = 0
@@ -106,6 +106,13 @@ class Screen:
             mistakes = 0
         return mistakes
 
+    # def show_mistake(self, number, letter):
+    #     self.generated_text.configure(state="normal")
+    #     self.generated_text.delete(number, number)
+    #     self.generated_text.insert(number, number, letter)
+    #     self.generated_text.tag_add("red", f"{self.set_of_words[self.words_points - 1][number]}")
+    #     self.generated_text.configure(state="disabled")
+
     # def count_words(self):
     #     # if keyboard.read_key() == "space":
     #     #     self.words_points += 1
@@ -135,6 +142,7 @@ class Screen:
                     self.mistakes = self.count_mistakes()
                 else:
                     self.spelling.append("wrong")
+                    # self.show_mistake(number, keyboard.read_key())
                     self.spelling_points = self.count_points()
                     self.mistakes = self.count_mistakes()
                 # if letters_string[len(letters_string) - 1] == self.text[len(letters_string) - 1]:
@@ -159,29 +167,27 @@ class Screen:
             self.timer_text['text'] = count
         else:
             self.window.after_cancel(self.time)
-            messagebox.showinfo("End", f"Your CPM is {int(self.points)}.\n"
-                                       f"Your WPM is {int(self.points / 5)},\n"
-                                       f"Net WPN is "
-                                       f"{int(self.points / 5) - self.mistakes}.\n"
-                                       f"Accuracy was "
-                                       f"{'%.2f' % (self.spelling_points / len(self.text.replace(' ', '')) * 100)}%.")
+            self.count_score()
 
     def start_timer(self):
         countdown = len(self.entry_txt) == 1
         if countdown:
             self.count_down(60)
         elif len(self.entry_txt) == len(self.text):
-            net = (int(((self.points / 5) - (self.mistakes * ((60 - int(self.timer_text['text'])) / 60))) /
-                       ((60 - int(self.timer_text['text'])) / 60)))
-            messagebox.showinfo("End", f"Your CPM is {int(self.points)}.\n"
-                                       f"Your WPM is "
-                                       f"{int((self.points / 5) / ((60 - int(self.timer_text['text'])) / 60))},\n"
-                                       f"Net WPN is {net}.\n"
-                                       f"Accuracy was "
-                                       f"{'%.2f' % (self.spelling_points / len(self.text.replace(' ', '')) * 100)}%.")
+            self.count_score()
 
     def count_score(self):
-        pass
+        cpm = int(self.points)
+        wpm = int((self.points / 5) / ((60 - int(self.timer_text['text'])) / 60))
+        net_wpm = (int(((self.points / 5) - (self.mistakes * ((60 - int(self.timer_text['text'])) / 60))) /
+                       ((60 - int(self.timer_text['text'])) / 60)))
+        accuracy = (self.spelling_points / len(self.text.replace(' ', '')) * 100)
+        messagebox.showinfo("End", f"Your CPM is {cpm}.\n"
+                            f"Your WPM is "
+                            f"{wpm},\n"
+                            f"Net WPN is {net_wpm}.\n"
+                            f"Accuracy was "
+                            f"{'%.2f' % accuracy}%.")
 
     def save_score(self):
         pass
