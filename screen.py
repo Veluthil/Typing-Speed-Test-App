644f1b54022040fadceecaf7d6a7e40a708796f7
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-03-03 19:01:54 +0100

    Add an instruction.

--- screen.py
+++ screen.py
@@ -15,6 +15,7 @@ class Screen:
 
         self.easy = Button()
         self.hard = Button()
+        self.info = Button()
         self.difficulty_label = Label()
         self.generated_text = Text()
         self.entry_field = Entry()
@@ -43,18 +44,52 @@ class Screen:
         self.wpm_value = Label()
         self.cpm_value = Label()
 
+        # messagebox.showinfo("Welcome in Typing Speed Test App",
+        #                     "This app allows you to check your typing speed.\n"
+        #                     "First, choose a difficulty.\n"
+        #                     "Then, you will be prompted with randomly chosen words of given difficulty.\n"
+        #                     "Once you start typing, the timer will start.\n"
+        #                     "Only after rewriting the whole text the test will be over and your results will be "
+        #                     "provided to you.\n"
+        #                     "Each mistake is counted, but it is allowed to fix them without points penalty.\n"
+        #                     "After each written word you must type a single space.")
         self.choose_difficulty()
         self.window.mainloop()
 
     def choose_difficulty(self):
-        self.difficulty_label = Label(text="CHOOSE A DIFFICULTY", font=("Tahoma", 20, "bold"), bg="#000000", fg="#fafafa")
-        self.difficulty_label.grid(column=0, row=0, columnspan=2, padx=50, pady=50)
-        self.easy = Button(text="EASY", font=("Tahoma", 12, "bold"), bg="#000000", fg="#fafafa",
+        self.difficulty_label = Label(text="CHOOSE A DIFFICULTY", font=("Tahoma", 25, "bold"), bg="#000000",
+                                      fg="#fafafa")
+        self.difficulty_label.grid(column=0, row=0, columnspan=3, padx=50, pady=20)
+        self.easy = Button(text="EASY", font=("Tahoma", 20, "bold"), bg="#000000", fg="#2AAA8A",
                            command=self.create_text_easy)
         self.easy.grid(column=0, row=1, padx=20, pady=20)
-        self.hard = Button(text="HARD", font=("Tahoma", 12, "bold"), bg="#000000", fg="#fafafa",
+        self.hard = Button(text="HARD", font=("Tahoma", 20, "bold"), bg="#000000", fg="#ff0000",
                            command=self.create_text_hard)
-        self.hard.grid(column=1, row=1, padx=20, pady=20)
+        self.hard.grid(column=2, row=1, padx=20, pady=20)
+        self.info = Button(text="INSTRUCTION", font=("Tahoma", 8, "bold"), bg="#000000", fg="#767676",
+                           command=lambda: [self.show_info(), self.info.destroy()])
+        self.info.grid(column=1, row=2)
+
+    def show_info(self):
+        self.easy.destroy()
+        self.hard.destroy()
+        self.difficulty_label.config(text="Welcome in Typing Speed Test App")
+        info_text = Label(font=("Tahoma", 14), bg="#000000", fg="#fafafa", anchor=W,
+                          text="This app allows you to check your typing speed. \n"
+                               "First, choose a difficulty. \n"
+                               "Then, the randomly chosen words of a given difficulty will appear. \n"
+                               "Once you start typing, the timer will start. \n"
+                               "After each word, type a SINGLE SPACE. After rewriting the whole text, \n"
+                               "the test will end, and the app will count and provide you with the final results. \n"
+                               "Each mistake counts, but you can fix them without a points penalty. \n"
+                               "The application automatically saves the highest score. \nGOOD LUCK!")
+        info_text.grid(column=0, row=1, pady=20)
+        back = Button(text="GO BACK", font=("Tahoma", 10, "bold"), bg="#000000", fg="#fafafa",
+                      command=lambda: [self.clear_screen(),
+                                       self.choose_difficulty(),
+                                       info_text.destroy(), back.destroy(),
+                                       self.difficulty_label.config(text="CHOOSE A DIFFICULTY")])
+        back.grid(column=0, row=2)
 
     def create_text_easy(self):
         self.clear_screen()
@@ -90,6 +125,7 @@ class Screen:
         self.text = ""
         self.easy.destroy()
         self.hard.destroy()
+        self.info.destroy()
         self.generated_text.destroy()
         self.spelling.clear()
         self.entry_field.delete(0, END)
@@ -238,14 +274,14 @@ class Screen:
         cpm = int(self.points)
         wpm = int((self.points / 5) / ((int(self.timer_text['text'])) / 60))
         net_wpm = (int(((self.points / 5) - (self.mistakes * ((int(self.timer_text['text'])) / 60))) /
-                                ((int(self.timer_text['text'])) / 60)))
+                       ((int(self.timer_text['text'])) / 60)))
         accuracy = (net_wpm * 100) / wpm
         messagebox.showinfo("End", f"Your CPM is {cpm}.\n"
-                            f"Your WPM is "
-                            f"{wpm},\n"
-                            f"Net WPN is {net_wpm}.\n"
-                            f"Accuracy was "
-                            f"{'%.2f' % accuracy}%.")
+                                   f"Your WPM is "
+                                   f"{wpm},\n"
+                                   f"Net WPN is {net_wpm}.\n"
+                                   f"Accuracy was "
+                                   f"{'%.2f' % accuracy}%.")
         self.save_score(self.net_wpm)
 
     def save_score(self, net_wpn):
@@ -265,27 +301,27 @@ class Screen:
     def create_widgets(self):
         # timer
         timer_label = Label(text="TIME:", fg="#fafafa", bg="#000000", font=("Tahoma", 12))
-        timer_label.grid(column=0, row=1, sticky=E)
+        timer_label.grid(column=0, row=0, sticky=E)
         self.timer_text = Label(text="0", fg="#fafafa", bg="#000000", font=("Tahoma", 12))
-        self.timer_text.grid(column=1, row=1, sticky=W)
+        self.timer_text.grid(column=1, row=0, sticky=W)
 
         # characters per minute
         cpm_label = Label(text=f"CPM: ", fg="#fafafa", bg="#000000", font=("Tahoma", 12))
-        cpm_label.grid(column=2, row=1, sticky=E)
+        cpm_label.grid(column=2, row=0, sticky=E)
         self.cpm_value = Label(text="0", fg="#fafafa", bg="#000000", font=("Tahoma", 12))
-        self.cpm_value.grid(column=3, row=1, sticky=W)
+        self.cpm_value.grid(column=3, row=0, sticky=W)
 
         # words per minute
         wpm_label = Label(text="WPM/NET WPM: ", fg="#fafafa", bg="#000000", font=("Tahoma", 12))
-        wpm_label.grid(column=4, row=1, sticky=E)
+        wpm_label.grid(column=4, row=0, sticky=E)
         self.wpm_value = Label(text="0", fg="#fafafa", bg="#000000", font=("Tahoma", 12))
-        self.wpm_value.grid(column=5, row=1, sticky=W)
+        self.wpm_value.grid(column=5, row=0, sticky=W)
 
         # mistakes
         mist_label = Label(text="MISTAKES: ", fg="#fafafa", bg="#000000", font=("Tahoma", 12))
-        mist_label.grid(column=6, row=1, sticky=E)
+        mist_label.grid(column=6, row=0, sticky=E)
         self.mist_value = Label(text="0", fg="#fafafa", bg="#000000", font=("Tahoma", 12))
-        self.mist_value.grid(column=7, row=1, sticky=W)
+        self.mist_value.grid(column=7, row=0, sticky=W)
 
         # current highest score
         high_score_label = Label(text=f"HIGH SCORE: {self.high_score} NET WPM.",
